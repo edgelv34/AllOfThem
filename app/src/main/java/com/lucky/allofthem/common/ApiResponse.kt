18b7548465e6bb10ea193019183a31ccf38bbe56
edgelv34 <edgelv34@gmail.com>
2025-04-25 16:33:26 +0900

    ApiResponse 구조 잡기

---
+++ app/src/main/java/com/lucky/allofthem/common/ApiResponse.kt
@@ -0,0 +1,26 @@
+package com.lucky.allofthem.common
+
+import okio.IOException
+import retrofit2.HttpException
+
+sealed interface ApiResponse<out T> {
+    data class Success<out T>(val data: T): ApiResponse<T>
+    data object NetworkError : ApiResponse<Nothing>
+    data object Unauthorized : ApiResponse<Nothing>
+    data class Failure(val message: String) : ApiResponse<Nothing>
+}
+
+
+suspend inline fun <T> safeApiCall(crossinline block: suspend () -> T): ApiResponse<T> {
+    return try {
+        ApiResponse.Success(block())
+    } catch (e: HttpException) {
+        ApiResponse.Unauthorized
+    } catch (e: IOException) {
+        ApiResponse.NetworkError
+    } catch (e: Exception) {
+        ApiResponse.Failure(message = "Unexpected error: ${e.message}")
+    }
+}
+
+
--- app/src/main/java/com/lucky/allofthem/common/AppError.kt
+++
@@ -1,7 +0,0 @@
-package com.lucky.allofthem.common
-
-sealed class AppError {
-    object NetworkError : AppError()
-    object Unauthorized : AppError()
-    data class Unknown(val message: String) : AppError()
-}
